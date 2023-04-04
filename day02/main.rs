b39585c01601e0d4d4d6448aab96f163f9c2ecd9
Seng <engelhardt_stefan@gmx.de>
2023-04-04 18:47:27 +0200

    solved day 2 part 2

--- day02/main.rs
+++ day02/main.rs
@@ -21,6 +21,19 @@ fn main() -> io::Result<()> {
         .map(|v| v.iter().max().unwrap() - v.iter().min().unwrap())
         .sum::<u32>();
 
+    let mut part2 = 0;
+    for line in number_lines {
+        for i in 0..line.len() {
+            for j in 0..line.len() {
+                if line[i] > line[j] && line[i] % line[j] == 0 {
+                    part2 += line[i] / line[j]
+                }
+            }
+        }
+    }
+
     println!("{}", part1);
+    println!("{}", part2);
+
     return Ok(());
 }
