8082b4f88dab460c7aee4a85f53eb9f5759b24ac
Seng <engelhardt_stefan@gmx.de>
2023-04-08 11:02:44 +0200

    added actual solution for day 3 part 2

--- day03/main.c
+++ day03/main.c
@@ -59,12 +59,97 @@ taxi_cab:
   return taxicab_distance((position){0, 0}, pos) - 1;
 }
 
+const position DIRECTIONS[] = {
+    {1, 0},  // right
+    {0, -1}, // top
+    {-1, 0}, // left
+    {0, 1},  // down
+};
+
+#define POS_L ((position){-1, 0})
+#define POS_T ((position){0, -1})
+#define POS_R ((position){1, 0})
+#define POS_B ((position){0, 1})
+#define POS_TL ((position){-1, -1})
+#define POS_TR ((position){1, -1})
+#define POS_BL ((position){-1, 1})
+#define POS_BR ((position){1, 1})
+
+const position CHECKS[4][4] = {
+    {POS_L, POS_TL, POS_T, POS_TR}, // going right
+    {POS_B, POS_BL, POS_L, POS_TL}, // going up
+    {POS_R, POS_BR, POS_B, POS_BL}, // going left
+    {POS_T, POS_TR, POS_R, POS_BR}, // going down,
+};
+
+const position EMPTY_POS = {INT32_MAX, INT32_MAX};
+
+static inline uint32_t calc_position_hash(const position *const p) {
+  return 54812489 * ((uint32_t)(p->x) ^ 95723417) *
+         ((uint32_t)(p->y) ^ 74297323);
+}
+
+static inline bool position_equals(const position *const a,
+                                   const position *const b) {
+  return a->x == b->x && a->y == b->y;
+}
+
+#define AOC_KEY_T position
+#define AOC_KEY_T_NAME Pos
+#define AOC_VALUE_T int32_t
+#define AOC_VALUE_T_NAME I32
+#define AOC_KEY_T_EMPTY EMPTY_POS
+#define AOC_KEY_T_HFUNC calc_position_hash
+#define AOC_KEY_T_EQUALS position_equals
+#define AOC_BASE2_CAPACITY
+#include <aoc/hashmap.h>
+
+static int32_t solve_part2(const int32_t input) {
+  AocHashmapPosI32 positions = {0};
+  AocHashmapPosI32Create(&positions, 64);
+
+  int32_t inc = 1;
+  int32_t solution = 0;
+  position current = {0, 0};
+  AocHashmapPosI32Insert(&positions, current, 1);
+
+  for (;;) {
+    for (int32_t d = 0; d < 4; ++d) {
+      for (int32_t i = 0; i < inc; ++i) {
+        current.x += DIRECTIONS[d].x;
+        current.y += DIRECTIONS[d].y;
+        int32_t sum = 0;
+        for (size_t c = 0; c < 4; ++c) {
+          int32_t value = 0;
+          const position p = {
+              current.x + CHECKS[d][c].x,
+              current.y + CHECKS[d][c].y,
+          };
+          if (AocHashmapPosI32Get(&positions, p, &value)) {
+            sum += value;
+          }
+        }
+        AocHashmapPosI32Insert(&positions, current, sum);
+        if (sum > input) {
+          solution = sum;
+          goto finish;
+        }
+      }
+      if (d % 2 != 0) // after every 2 turns increase the amount
+        inc++;
+    }
+  }
+
+finish:
+  AocHashmapPosI32Destroy(&positions);
+  return solution;
+}
+
 int main(void) {
   const int32_t input = 361527;
 
   const int32_t part1 = solve_part1(input);
-  // cheated for now. might come back to it later: https://oeis.org/A141481
-  const int32_t part2 = 363010;
+  const int32_t part2 = solve_part2(input);
 
   printf("%d\n", part1);
   printf("%d\n", part2);
