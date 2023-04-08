79a38c7c3dc31f0fb88039b6b8ddfa0d6ff353e5
Seng <engelhardt_stefan@gmx.de>
2023-04-09 00:59:32 +0200

    solved day 5 part 1

---
+++ day05/main.c
@@ -0,0 +1,36 @@
+#include <aoc/aoc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define AOC_T int32_t
+#define AOC_T_NAME I32
+#include <aoc/array.h>
+
+static void parse_line(char *line, size_t length, void *userData) {
+  AocArrayI32 *offsets = userData;
+  AocArrayI32Push(offsets, strtol(line, NULL, 10));
+}
+
+static uint32_t solve_part1(AocArrayI32 *const offsets) {
+  uint32_t steps = 0;
+  const int32_t *end = offsets->items + offsets->length;
+  register int32_t *pc = offsets->items;
+  while (pc < end) {
+    (*pc)++;
+    pc += (*pc - 1);
+    steps++;
+  }
+  return steps;
+}
+
+int main(void) {
+  AocArrayI32 offsets = {0};
+  AocArrayI32Create(&offsets, 1080);
+  AocReadFileLineByLine("day05/input.txt", parse_line, &offsets);
+
+  const uint32_t part1 = solve_part1(&offsets);
+
+  printf("%u\n", part1);
+
+  AocArrayI32Destroy(&offsets);
+}
