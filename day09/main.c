19ddf3dd17c58d9476a07a06a903164941ab701b
Seng <engelhardt_stefan@gmx.de>
2023-04-11 20:11:25 +0200

    solved day 9 part 2

--- day09/main.c
+++ day09/main.c
@@ -3,14 +3,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static void parse_garbage(char *str, char **end) {
+static void parse_garbage(char *str, char **end, uint32_t *const part2) {
   while (*str != '>') {
-    str += (*str == '!' ? 2 : 1);
+    if (*str == '!') {
+      str += 2;
+    } else {
+      str++;
+      (*part2)++;
+    }
   }
   *end = str;
 }
 
-static uint32_t parse_group(char *str, char **end, uint32_t depthScore) {
+static uint32_t parse_group(char *str, char **end, uint32_t depthScore,
+                            uint32_t *const part2) {
   uint32_t score = depthScore;
 
   do {
@@ -19,12 +25,12 @@ static uint32_t parse_group(char *str, char **end, uint32_t depthScore) {
     switch (*str) {
     case '{':
       str++; // {
-      score += parse_group(str, &str, depthScore + 1);
+      score += parse_group(str, &str, depthScore + 1, part2);
       str++; // }
       break;
     case '<':
       str++; // <
-      parse_garbage(str, &str);
+      parse_garbage(str, &str, part2);
       str++; // >
       break;
     }
@@ -34,8 +40,8 @@ static uint32_t parse_group(char *str, char **end, uint32_t depthScore) {
   return score;
 }
 
-static uint32_t solve_part1(char *str) {
-  return parse_group(str, &str, 0);
+static void solve(char *str, uint32_t *const part1, uint32_t *const part2) {
+  *part1 = parse_group(str, &str, 0, part2);
 }
 
 int main(void) {
@@ -43,9 +49,12 @@ int main(void) {
   size_t length = 0;
   AocReadFileToString("day09/input.txt", &input, &length);
 
-  uint32_t part1 = solve_part1(input);
+  uint32_t part1 = 0;
+  uint32_t part2 = 0;
+  solve(input, &part1, &part2);
 
   printf("%u\n", part1);
+  printf("%u\n", part2);
 
   free(input);
 }
