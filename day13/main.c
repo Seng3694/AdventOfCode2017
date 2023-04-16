85715b30eaf98d967e560b4d2f32ddbb2db9eebb
Seng <engelhardt_stefan@gmx.de>
2023-04-16 13:48:55 +0200

    solved day 13 part 2

--- day13/main.c
+++ day13/main.c
@@ -2,66 +2,62 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define LAYERS_MAX 100
-
-static void parse_line(char *line, size_t length, void *userData) {
-  uint8_t *const ranges = userData;
-  const uint8_t id = strtol(line, &line, 10);
-  ranges[id] = strtol(line + 2, NULL, 10);
-}
-
-typedef enum {
-  DIRECTION_DOWN = 1,
-  DIRECTION_UP = -1,
-} direction;
+typedef struct {
+  int8_t scannerCache[64];
+  int8_t cycleLength;
+  int8_t depth;
+  int8_t range;
+} layer;
 
 typedef struct {
-  int8_t position;
-  int8_t direction;
-} scanner;
+  layer layers[64];
+  uint8_t count;
+} context;
 
-static void move_scanner(scanner *const s, const uint8_t range) {
-  const int8_t newPos = s->position + s->direction;
-  if (newPos < 0) {
-    s->position = 1;
-    s->direction = DIRECTION_DOWN;
-  } else if (newPos == range) {
-    s->position = range - 2;
-    s->direction = DIRECTION_UP;
-  } else {
-    s->position = newPos;
-  }
+static inline int8_t calc_cycle_length(const int8_t range) {
+  return (range - 1) * 2;
 }
 
-static void tick(const uint8_t ranges[const LAYERS_MAX],
-                 scanner scanners[const LAYERS_MAX]) {
-  for (uint8_t i = 0; i < LAYERS_MAX; ++i) {
-    if (ranges[i] > 0) {
-      move_scanner(&scanners[i], ranges[i]);
-    }
-  }
+static void parse_line(char *line, size_t length, void *userData) {
+  context *const ctx = userData;
+  layer *const current = &ctx->layers[ctx->count];
+  current->depth = strtol(line, &line, 10);
+  current->range = strtol(line + 2, NULL, 10);
+  current->cycleLength = calc_cycle_length(current->range);
+  for (int8_t i = 0; i < current->cycleLength; ++i)
+    current->scannerCache[i] = (i + current->depth) % current->cycleLength;
+  ctx->count++;
 }
 
-static uint32_t solve_part1(const uint8_t ranges[const LAYERS_MAX]) {
-  scanner scanners[LAYERS_MAX] = {0};
-  for (uint8_t i = 0; i < LAYERS_MAX; ++i)
-    scanners[i].direction = DIRECTION_DOWN;
-
+static uint32_t solve_part1(const context *const ctx) {
   uint32_t severity = 0;
-  for (uint8_t position = 0; position < LAYERS_MAX; ++position) {
-    if (ranges[position] > 0 && scanners[position].position == 0)
-      severity += (position * ranges[position]);
-    tick(ranges, scanners);
+  for (uint8_t i = 0; i < ctx->count; ++i) {
+    const layer *const current = &ctx->layers[i];
+    if (current->scannerCache[0] == 0)
+      severity += current->depth * current->range;
   }
-
   return severity;
 }
 
-int main(void) {
-  uint8_t ranges[LAYERS_MAX] = {0};
-  AocReadFileLineByLine("day13/input.txt", parse_line, ranges);
+static uint32_t solve_part2(const context *const ctx) {
+  for (uint32_t delay = 1;; ++delay) {
+    for (uint8_t i = 0; i < ctx->count; ++i) {
+      const layer *const current = &ctx->layers[i];
+      if (current->scannerCache[delay % current->cycleLength] == 0)
+        goto next;
+    }
+    return delay;
+  next:;
+  }
+}
 
-  const uint32_t part1 = solve_part1(ranges);
+int main(void) {
+  context ctx = {0};
+  AocReadFileLineByLine("day13/input.txt", parse_line, &ctx);
+
+  const uint32_t part1 = solve_part1(&ctx);
+  const uint32_t part2 = solve_part2(&ctx);
 
   printf("%u\n", part1);
+  printf("%u\n", part2);
 }
