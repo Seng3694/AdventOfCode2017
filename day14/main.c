0aef58be2bc47dc0bf6de959871a70051e423d28
Seng <engelhardt_stefan@gmx.de>
2023-06-23 19:26:05 +0200

    solved day 14 part 2

--- day14/main.c
+++ day14/main.c
@@ -69,36 +69,69 @@ static void knot_hash(uint8_t *const lengths, const size_t count,
   create_hash(string, hash);
 }
 
-static uint32_t solve_part1(const char *const input) {
+static void mark_region(uint32_t *const map, const uint32_t x, const uint32_t y,
+                        const uint32_t marker) {
+  if (map[y * 128 + x] != 1)
+    return;
+
+  map[y * 128 + x] = marker;
+
+  if (x > 0)
+    mark_region(map, x - 1, y + 0, marker); // left
+  if (y > 0)
+    mark_region(map, x + 0, y - 1, marker); // top
+  if (x < 127)
+    mark_region(map, x + 1, y + 0, marker); // right
+  if (y < 127)
+    mark_region(map, x + 0, y + 1, marker); // bottom
+}
+
+static uint32_t create_regions(uint32_t *const map) {
+  // map has only ones and zeros. start marking groups with 2 and onwards
+  uint32_t regionCount = 0;
+  for (uint32_t y = 0; y < 128; ++y) {
+    for (uint32_t x = 0; x < 128; ++x) {
+      uint32_t *const current = &map[y * 128 + x];
+      if (*current == 1) {
+        mark_region(map, x, y, regionCount + 2); // +2 to skip 0 and 1
+        regionCount++;
+      }
+    }
+  }
+  return regionCount;
+}
+
+static void solve_both(const char *const input, uint32_t *const part1,
+                       uint32_t *const part2) {
   char buffer[32] = {0};
   uint8_t hashBuffer[32] = {0};
+  uint32_t map[128 * 128] = {0};
 
-  uint32_t active = 0;
+  uint32_t totalUsed = 0;
   for (uint32_t row = 0; row < 128; ++row) {
     snprintf(buffer, 32, "%s-%u", input, row);
     size_t length = strlen(buffer);
     add_suffix(buffer, &length);
     knot_hash((uint8_t *)buffer, length, hashBuffer);
-    for (uint32_t i = 0; i < 32; i += 8) {
-      const uint32_t part = (uint32_t)hashBuffer[i + 7] |
-                            ((uint32_t)hashBuffer[i + 6] << 4) |
-                            ((uint32_t)hashBuffer[i + 5] << 8) |
-                            ((uint32_t)hashBuffer[i + 4] << 12) |
-                            ((uint32_t)hashBuffer[i + 3] << 16) |
-                            ((uint32_t)hashBuffer[i + 2] << 20) |
-                            ((uint32_t)hashBuffer[i + 1] << 24) |
-                            ((uint32_t)hashBuffer[i + 0] << 28);
-      active += AocPopCnt32(part);
+    for (uint8_t i = 0; i < 32; ++i) {
+      for (uint8_t j = 0; j < 4; ++j) {
+        const uint8_t isUsed = AOC_CHECK_BIT(hashBuffer[i], (3 - j));
+        totalUsed += isUsed;
+        map[row * 128 + (i * 4) + j] = isUsed;
+      }
     }
   }
 
-  return active;
+  *part1 = totalUsed;
+  *part2 = create_regions(map);
 }
 
 int main(void) {
   const char input[] = "ffayrhll";
-
-  const uint32_t part1 = solve_part1(input);
+  uint32_t part1 = 0;
+  uint32_t part2 = 0;
+  solve_both(input, &part1, &part2);
 
   printf("%u\n", part1);
+  printf("%u\n", part2);
 }
