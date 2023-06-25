b0a97d615c1802df2a197dc63eae9e67fc4dbf1a
Seng <engelhardt_stefan@gmx.de>
2023-06-25 12:53:02 +0200

    solved day 15 part 2

--- day15/main.c
+++ day15/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <aoc/aoc.h>
 
+#define MOD_BASE2(a, b) ((a) & ((b)-1))
+
 static const uint64_t FACTOR_A = 16807;
 static const uint64_t FACTOR_B = 48271;
 
@@ -40,10 +42,37 @@ static uint32_t solve_part1(const context *const ctx, const uint32_t runs) {
   return matchCount;
 }
 
+static uint32_t solve_part2(const context *const ctx, const uint32_t runs) {
+  uint64_t previousValueA = ctx->startValueA;
+  uint64_t previousValueB = ctx->startValueB;
+  uint64_t currentValueA = previousValueA;
+  uint64_t currentValueB = previousValueB;
+  uint32_t matchCount = 0;
+
+  for (uint32_t i = 0; i < runs; ++i) {
+    do {
+      previousValueA = currentValueA;
+      currentValueA = (previousValueA * FACTOR_A) % 2147483647;
+    } while (MOD_BASE2(currentValueA, 4) != 0);
+
+    do {
+      previousValueB = currentValueB;
+      currentValueB = (previousValueB * FACTOR_B) % 2147483647;
+    } while (MOD_BASE2(currentValueB, 8) != 0);
+
+    if ((currentValueA & 0xffff) == (currentValueB & 0xffff))
+      matchCount++;
+  }
+
+  return matchCount;
+}
+
 int main(void) {
   context ctx = {0};
   AocReadFileLineByLineEx("day15/input.txt", parse, &ctx);
 
   const uint32_t part1 = solve_part1(&ctx, 40000000);
+  const uint32_t part2 = solve_part2(&ctx, 5000000);
   printf("%u\n", part1);
+  printf("%u\n", part2);
 }
