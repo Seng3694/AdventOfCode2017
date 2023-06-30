da5254239aa0eca41f9ae935890e4a27feb5ab68
Seng <engelhardt_stefan@gmx.de>
2023-06-30 21:51:06 +0200

    solved day 20 part 2

--- day20/main.c
+++ day20/main.c
@@ -103,14 +103,76 @@ static uint32_t solve_part1(AocArrayParticle *particles) {
   return particles->items[0].index;
 }
 
+#define AOC_T int32_t
+#define AOC_T_NAME I32
+#include <aoc/array.h>
+
+static inline int i32_compare(const void *const a, const void *const b) {
+  return (*(int32_t *)a - *(int32_t *)b);
+}
+
+static uint32_t solve_part2(AocArrayParticle *const particles) {
+  AocArrayI32 collisions = {0};
+  AocArrayI32Create(&collisions, particles->capacity);
+
+  const uint32_t ticks = 100; // arbitrary. with the input the actual last
+                              // collision happens at tick 40
+  for (uint32_t t = 0; t < ticks; ++t) {
+    // check for collision
+    AocArrayI32Clear(&collisions);
+    for (size_t i = 0; i < particles->length - 1; ++i) {
+      const particle *const a = &particles->items[i];
+      for (size_t j = i + 1; j < particles->length; ++j) {
+        const particle *const b = &particles->items[j];
+        if (a->position.x == b->position.x && a->position.y == b->position.y &&
+            a->position.z == b->position.z) {
+          AocArrayI32Push(&collisions, (uint32_t)i);
+          AocArrayI32Push(&collisions, (uint32_t)j);
+        }
+      }
+    }
+
+    // "remove" particles when they collided
+    if (collisions.length > 0) {
+      qsort(collisions.items, collisions.length, sizeof(uint32_t), i32_compare);
+      int32_t current = -1; // for skipping duplicates
+      for (size_t i = 0; i < collisions.length; ++i) {
+        if (current != collisions.items[i]) {
+          // move particle at the end to position to delete
+          particles->items[collisions.items[i]] =
+              particles->items[particles->length - 1];
+          particles->length--;
+          current = collisions.items[i];
+        }
+      }
+    }
+
+    // update movement
+    for (size_t i = 0; i < particles->length; ++i) {
+      particle *const p = &particles->items[i];
+      p->velocity.x += p->acceleration.x;
+      p->velocity.y += p->acceleration.y;
+      p->velocity.z += p->acceleration.z;
+      p->position.x += p->velocity.x;
+      p->position.y += p->velocity.y;
+      p->position.z += p->velocity.z;
+    }
+  }
+
+  AocArrayI32Destroy(&collisions);
+  return (uint32_t)particles->length;
+}
+
 int main(void) {
   AocArrayParticle particles = {0};
   AocArrayParticleCreate(&particles, 1000);
   AocReadFileLineByLineEx("day20/input.txt", parse, &particles);
 
-  uint32_t part1 = solve_part1(&particles);
+  const uint32_t part1 = solve_part1(&particles);
+  const uint32_t part2 = solve_part2(&particles);
 
   printf("%u\n", part1);
+  printf("%u\n", part2);
 
   AocArrayParticleDestroy(&particles);
 }
