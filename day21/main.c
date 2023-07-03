9f08b49ceaff6936d134d284c8662c81500a55a8
Seng <engelhardt_stefan@gmx.de>
2023-07-03 20:52:09 +0200

    solved day 21 part 2

--- day21/main.c
+++ day21/main.c
@@ -4,31 +4,19 @@
 #include <string.h>
 
 typedef struct {
-  uint16_t size;
-  uint16_t data;
+  uint32_t size;
+  uint32_t data;
 } pattern;
 
 const pattern EMPTY_PATTERN = {0};
 
 static inline uint32_t calc_pattern_hash(const pattern *const p) {
-  return 54812489 * ((uint32_t)p->size ^ 95723417) *
-         ((uint32_t)p->data ^ 74297323);
+  return 54812489 * (p->size ^ 95723417) * (p->data ^ 74297323);
 }
 
 static inline bool pattern_equals(const pattern *const a,
                                   const pattern *const b) {
   return a->size == b->size && a->data == b->data;
-}
-
-static void print_pattern(const pattern p) {
-  printf("size: %u, hash: %8x\n", p.size, calc_pattern_hash(&p));
-  for (uint16_t y = 0; y < p.size; ++y) {
-    for (uint16_t x = 0; x < p.size; ++x) {
-      const uint16_t index = y * p.size + x;
-      printf("%c ", AOC_CHECK_BIT(p.data, index) ? '#' : '.');
-    }
-    printf("\n");
-  }
 }
 
 #define AOC_KEY_T pattern
@@ -44,11 +32,11 @@ static void print_pattern(const pattern p) {
 #define SET_BIT(value, bit, c)                                                 \
   c == '#' ? AOC_SET_BIT(value, bit) : AOC_CLEAR_BIT(value, bit)
 
-static pattern parse_pattern(char *line, char **out, const uint16_t size) {
+static pattern parse_pattern(char *line, char **out, const uint32_t size) {
   pattern p = {.size = size};
-  const uint16_t area = size * size;
-  for (uint16_t i = 0; i < area; i += size) {
-    for (uint16_t j = 0; j < size; ++j) {
+  const uint32_t area = size * size;
+  for (uint32_t i = 0; i < area; i += size) {
+    for (uint32_t j = 0; j < size; ++j) {
       p.data = SET_BIT(p.data, i + j, *(line + i + j + (i / size)));
     }
   }
@@ -66,10 +54,10 @@ static pattern rotate_right(const pattern p) {
     678    852
   */
   pattern out = {.size = p.size};
-  for (uint16_t y = 0; y < p.size; ++y) {
-    for (uint16_t x = 0; x < p.size; ++x) {
-      const uint16_t from = y * p.size + x;
-      const uint16_t to = x * p.size + (p.size - y - 1);
+  for (uint32_t y = 0; y < p.size; ++y) {
+    for (uint32_t x = 0; x < p.size; ++x) {
+      const uint32_t from = y * p.size + x;
+      const uint32_t to = x * p.size + (p.size - y - 1);
       if (AOC_CHECK_BIT(p.data, from))
         out.data = AOC_SET_BIT(out.data, to);
     }
@@ -84,10 +72,10 @@ static pattern flip_horizontal(const pattern p) {
     678    012
   */
   pattern out = {.size = p.size};
-  for (uint16_t y = 0; y < p.size; ++y) {
-    for (uint16_t x = 0; x < p.size; ++x) {
-      const uint16_t from = y * p.size + x;
-      const uint16_t to = (p.size - y - 1) * p.size + x;
+  for (uint32_t y = 0; y < p.size; ++y) {
+    for (uint32_t x = 0; x < p.size; ++x) {
+      const uint32_t from = y * p.size + x;
+      const uint32_t to = (p.size - y - 1) * p.size + x;
       if (AOC_CHECK_BIT(p.data, from))
         out.data = AOC_SET_BIT(out.data, to);
     }
@@ -120,8 +108,8 @@ static void generate_all_8_patterns(const pattern p, pattern b[const 8]) {
 
 static void parse(char *line, size_t length, void *userData) {
   AocHashmapPatternPattern *const map = userData;
-  const uint16_t fromSize = length == 21 ? 2 : 3;
-  const uint16_t toSize = length == 21 ? 3 : 4;
+  const uint32_t fromSize = length == 21 ? 2 : 3;
+  const uint32_t toSize = length == 21 ? 3 : 4;
   const pattern from = parse_pattern(line, &line, fromSize);
   const pattern to = parse_pattern(line + 4, &line, toSize);
 
@@ -137,7 +125,7 @@ static void parse(char *line, size_t length, void *userData) {
 
 typedef struct {
   char *data;
-  uint16_t size;
+  uint32_t size;
 } image;
 
 static uint32_t count_pixels(const image *const img) {
@@ -148,17 +136,6 @@ static uint32_t count_pixels(const image *const img) {
   return count;
 }
 
-static void print_image(const image *const img) {
-  printf("[img]: size:%u, active:%u\n", img->size, count_pixels(img));
-  for (uint16_t imgY = 0; imgY < img->size; ++imgY) {
-    for (uint16_t imgX = 0; imgX < img->size; ++imgX) {
-      printf("%c ", img->data[imgY * img->size + imgX]);
-    }
-    printf("\n");
-  }
-  printf("\n");
-}
-
 static void init_image(image *const img) {
   img->size = 3;
   img->data = malloc(sizeof(char) * img->size * img->size);
@@ -167,20 +144,20 @@ static void init_image(image *const img) {
 
 static void enhance(image *const img,
                     const AocHashmapPatternPattern *const map) {
-  const uint16_t size = img->size;
+  const uint32_t size = img->size;
 
   const uint32_t from = size % 2 == 0 ? 2 : 3;
   const uint32_t to = size % 2 == 0 ? 3 : 4;
 
   const uint32_t sizeNew = img->size + (img->size / from);
   char *data = malloc(sizeof(char) * sizeNew * sizeNew);
-  for (uint16_t y = 0; y < img->size; y += from) {
-    for (uint16_t x = 0; x < img->size; x += from) {
+  for (uint32_t y = 0; y < img->size; y += from) {
+    for (uint32_t x = 0; x < img->size; x += from) {
       pattern sourcePattern = {.size = from};
-      for (uint16_t y2 = 0; y2 < from; ++y2) {
-        for (uint16_t x2 = 0; x2 < from; ++x2) {
-          const uint16_t src = (y + y2) * img->size + (x + x2);
-          const uint16_t dest = y2 * from + x2;
+      for (uint32_t y2 = 0; y2 < from; ++y2) {
+        for (uint32_t x2 = 0; x2 < from; ++x2) {
+          const uint32_t src = (y + y2) * img->size + (x + x2);
+          const uint32_t dest = y2 * from + x2;
           sourcePattern.data =
               SET_BIT(sourcePattern.data, dest, img->data[src]);
         }
@@ -188,10 +165,10 @@ static void enhance(image *const img,
 
       pattern destPattern = {.size = to};
       AocHashmapPatternPatternGet(map, sourcePattern, &destPattern);
-      for (uint16_t y2 = 0; y2 < to; ++y2) {
-        for (uint16_t x2 = 0; x2 < to; ++x2) {
-          const uint16_t src = y2 * to + x2;
-          const uint16_t dest =
+      for (uint32_t y2 = 0; y2 < to; ++y2) {
+        for (uint32_t x2 = 0; x2 < to; ++x2) {
+          const uint32_t src = y2 * to + x2;
+          const uint32_t dest =
               (y + (y / from) + y2) * sizeNew + (x + (x / from) + x2);
           data[dest] = AOC_CHECK_BIT(destPattern.data, src) ? '#' : '.';
         }
@@ -204,18 +181,23 @@ static void enhance(image *const img,
   img->size = sizeNew;
 }
 
-uint32_t solve_part1(const AocHashmapPatternPattern *const map) {
+void solve_both(const AocHashmapPatternPattern *const map,
+                uint32_t *const part1, uint32_t *const part2) {
   image img = {0};
   init_image(&img);
 
-  for (uint32_t i = 0; i < 5; ++i) {
+  uint32_t i = 0;
+  for (; i < 5; ++i)
     enhance(&img, map);
-  }
 
-  const uint32_t enabledPixels = count_pixels(&img);
+  *part1 = count_pixels(&img);
+
+  for (; i < 18; ++i)
+    enhance(&img, map);
+
+  *part2 = count_pixels(&img);
 
   free(img.data);
-  return enabledPixels;
 }
 
 int main(void) {
@@ -223,9 +205,12 @@ int main(void) {
   AocHashmapPatternPatternCreate(&map, 128);
   AocReadFileLineByLine("day21/input.txt", parse, &map);
 
-  const uint32_t part1 = solve_part1(&map);
+  uint32_t part1 = 0;
+  uint32_t part2 = 0;
+  solve_both(&map, &part1, &part2);
 
   printf("%u\n", part1);
+  printf("%u\n", part2);
 
   AocHashmapPatternPatternDestroy(&map);
 }
