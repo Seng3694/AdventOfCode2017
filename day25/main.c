77969e4b7a0bc01d95e9e1f98b9a3c0c6e99b746
Seng <engelhardt_stefan@gmx.de>
2023-07-07 15:03:05 +0200

    changed fixed array to hashset

--- day25/main.c
+++ day25/main.c
@@ -103,21 +103,44 @@ static void parse(char *content, context *const ctx) {
   }
 }
 
+static inline uint32_t calc_i32_hash(const int32_t *const i) {
+  return 2166136261u ^ ((*(uint32_t *)i) * 16777619);
+}
+
+static inline bool i32_equals(const int32_t *const a, const int32_t *const b) {
+  return *a == *b;
+}
+
+#define AOC_T int32_t
+#define AOC_T_NAME I32
+#define AOC_T_EMPTY ((int32_t){INT32_MAX})
+#define AOC_T_HFUNC(x) calc_i32_hash(x)
+#define AOC_T_EQUALS(a, b) i32_equals(a, b)
+#define AOC_BASE2_CAPACITY
+#include <aoc/hashset.h>
+
 static size_t solve(const context *const ctx) {
   uint8_t currentStateIndex = ctx->startState;
-  int32_t position = 6000;
-  uint8_t values[12000] = {0};
+  AocHashsetI32 hs = {0};
+  AocHashsetI32Create(&hs, 8192);
+  int32_t position = 0;
+  uint32_t hash = 0;
 
   for (uint32_t i = 0; i < ctx->steps; ++i) {
     const state *const currentState = &ctx->states.items[currentStateIndex];
+    const bool contains = AocHashsetI32Contains(&hs, position, &hash);
     const instruction *const instructions =
-        values[position] == 1 ? currentState->ifOne : currentState->ifZero;
+        contains ? currentState->ifOne : currentState->ifZero;
     for (uint32_t j = 0; j < 3; ++j) {
       const instruction *const instr = &instructions[j];
       switch (instr->type) {
-      case INSTRUCTION_TYPE_WRITE: {
-        values[position] = instr->operand;
-      } break;
+      case INSTRUCTION_TYPE_WRITE:
+        if (!contains && instr->operand == 1) {
+          AocHashsetI32InsertPreHashed(&hs, position, hash);
+        } else if (contains && instr->operand == 0) {
+          AocHashsetI32RemovePreHashed(&hs, position, hash);
+        }
+        break;
       case INSTRUCTION_TYPE_MOVE:
         position +=
             (move_direction)instr->operand == MOVE_DIRECTION_LEFT ? -1 : 1;
@@ -129,12 +152,8 @@ static size_t solve(const context *const ctx) {
     }
   }
 
-  size_t solution = 0;
-  for (size_t i = 0; i < 12000; ++i)
-    if (values[i] == 1)
-      solution++;
-
-  return solution;
+  AocHashsetI32Destroy(&hs);
+  return hs.count;
 }
 
 int main(void) {
